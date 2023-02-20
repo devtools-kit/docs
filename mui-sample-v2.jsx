a545353333fa45efae88c266218ea34e70c63f0c
devtools-kit <125875189+devtools-kit@users.noreply.github.com>
2023-02-21 00:10:02 +0530

    Create mui-sample-v2.jsx

---
+++ mui-sample-v2.jsx
@@ -0,0 +1,400 @@
+import React, { useState } from 'react';
+import { alpha, createStyles, withStyles } from '@material-ui/core/styles';
+import {
+  Box,
+  Button,
+  InputBase,
+  Tab,
+  Tabs,
+} from '@material-ui/core';
+import styled from 'styled-components';
+
+const StyledTab = withStyles((theme) => ({
+  root: {
+    textTransform: 'initial'
+  },
+}))(Tab);
+
+const Label = styled.label`
+  margin-right: 10px;
+`;
+
+const FieldsContainer = styled.div`
+  display: flex;
+  flex-flow: column;
+  width: 400px;
+`;
+
+const FieldContainer = styled.div`
+  display: flex;
+  flex-flow: row;
+  & > * {
+    display: flex;
+    flex: 1;
+    justify-content: right;
+    align-items: center;
+  }
+  margin-bottom: 10px;
+`;
+
+const TextField = withStyles((theme) =>
+  createStyles({
+    input: {
+      borderRadius: 4,
+      position: 'relative',
+      backgroundColor: theme.palette.common.white,
+      border: '1px solid #ced4da',
+      fontSize: 16,
+      width: 'auto',
+      padding: '10px 12px',
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+      '&:focus': {
+        boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+        borderColor: theme.palette.primary.main,
+      },
+    },
+  })
+)(InputBase);
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div role="tabpanel" hidden={value !== index} {...other}>
+      {value === index && <Box p={3}>{children}</Box>}
+    </div>
+  );
+}
+
+const SubmitBtn = withStyles(() => ({
+  root: {
+    background: 'green',
+    color: 'white',
+  },
+}))(Button);
+
+const ActionsContainer = styled.div`
+  display: flex;
+  position: relative;
+  width: 100%;
+  padding: 10px 0px;
+  & > :last-child {
+    position: absolute;
+    right: 10px;
+  }
+  & > :first-child {
+    position: absolute;
+    left: 10px;
+  }
+`;
+
+const PanelTypes = {
+  PARTY_ID: 'Party ID',
+  SOCIAL_SECURITY_NUMBER_AND_DATE_OF_BIRTH:
+    'Social Security Number and Date of Birth',
+  CONSUMER_ID: 'Consumer ID',
+  FIRST_NAME_LAST_NAME_DATE_OF_BIRTH_AND_CONTRACT_ID:
+    'First Name, Last Name, Date of Birth and Contract ID',
+  CONTRACT_ID_AND_PERSON_NUMBER: 'Contract ID and Person Number',
+  FIRST_NAME_LAST_NAME_GROUP_NUMBER_AND_DATE_OF_BIRTH:
+    'First Name, Last Name, Group Number and Date of Birth (Only for BA Search)',
+};
+
+const defaultFormValue = {
+  partyId: '',
+  ssn: '',
+  dob: '',
+  consumerId: '',
+  firstName: '',
+  lastName: '',
+  contractId: '',
+  personNumber: '',
+  groupNumber: '',
+  workEmail: '',
+  workPhone: '',
+};
+
+export default function SearchOptions() {
+
+  const [formValue, setFormValue] = useState({ ...defaultFormValue });
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (_, newValue) => {
+    setValue(newValue);
+  };
+
+  const reset = () => {
+    setFormValue({ ...defaultFormValue });
+  };
+
+  const submit = () => {
+    console.log(formValue);
+  };
+
+  return (
+    <div>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        variant="fullWidth"
+      >
+        <StyledTab label={PanelTypes.PARTY_ID} />
+        <StyledTab
+          label={PanelTypes.SOCIAL_SECURITY_NUMBER_AND_DATE_OF_BIRTH}
+        />
+        <StyledTab label={PanelTypes.CONSUMER_ID} />
+        <StyledTab
+          label={PanelTypes.FIRST_NAME_LAST_NAME_DATE_OF_BIRTH_AND_CONTRACT_ID}
+        />
+        <StyledTab label={PanelTypes.CONTRACT_ID_AND_PERSON_NUMBER} />
+        <StyledTab
+          label={PanelTypes.FIRST_NAME_LAST_NAME_GROUP_NUMBER_AND_DATE_OF_BIRTH}
+        />
+      </Tabs>
+      <div>
+        <TabPanel value={value} index={0}>
+          <SearchForm
+            type={PanelTypes.PARTY_ID}
+            formValue={formValue}
+            setFormValue={setFormValue}
+          />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          <SearchForm
+            type={PanelTypes.SOCIAL_SECURITY_NUMBER_AND_DATE_OF_BIRTH}
+            formValue={formValue}
+            setFormValue={setFormValue}
+          />
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          <SearchForm
+            type={PanelTypes.CONSUMER_ID}
+            formValue={formValue}
+            setFormValue={setFormValue}
+          />
+        </TabPanel>
+        <TabPanel value={value} index={3}>
+          <SearchForm
+            type={PanelTypes.FIRST_NAME_LAST_NAME_DATE_OF_BIRTH_AND_CONTRACT_ID}
+            formValue={formValue}
+            setFormValue={setFormValue}
+          />
+        </TabPanel>
+        <TabPanel value={value} index={4}>
+          <SearchForm
+            type={PanelTypes.CONTRACT_ID_AND_PERSON_NUMBER}
+            formValue={formValue}
+            setFormValue={setFormValue}
+          />
+        </TabPanel>
+        <TabPanel value={value} index={5}>
+          <SearchForm
+            type={
+              PanelTypes.FIRST_NAME_LAST_NAME_GROUP_NUMBER_AND_DATE_OF_BIRTH
+            }
+            formValue={formValue}
+            setFormValue={setFormValue}
+          />
+        </TabPanel>
+      </div>
+      <ActionsContainer>
+        <Button variant="contained" onClick={(e) => reset()}>
+          Reset
+        </Button>
+        <SubmitBtn variant="contained" onClick={(e) => submit()}>
+          Submit
+        </SubmitBtn>
+      </ActionsContainer>
+    </div>
+  );
+}
+
+export function SearchForm({ type, formValue, setFormValue }) {
+  return (
+    <div>
+      {type === PanelTypes.PARTY_ID && (
+        <FieldsContainer>
+            <FieldContainer>
+            <Label>Party ID:</Label>
+            <TextField
+                value={formValue.partyId}
+                onChange={(e) => {
+                setFormValue({ ...formValue, partyId: e.target.value });
+                }}
+            />
+            </FieldContainer>
+        </FieldsContainer>
+      )}
+      {type === PanelTypes.SOCIAL_SECURITY_NUMBER_AND_DATE_OF_BIRTH && (
+        <FieldsContainer>
+          <FieldContainer>
+            <Label>Social Securiy Number:</Label>
+            <TextField
+              value={formValue.ssn}
+              onChange={(e) => {
+                setFormValue({ ...formValue, ssn: e.target.value });
+              }}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Date of Birth:</Label>
+            <TextField
+              value={formValue.dob}
+              onChange={(e) => {
+                setFormValue({ ...formValue, dob: e.target.value });
+              }}
+            />
+          </FieldContainer>
+        </FieldsContainer>
+      )}
+      {type === PanelTypes.CONSUMER_ID && (
+        <FieldsContainer>
+            <FieldContainer>
+            <Label>Consumer ID:</Label>
+            <TextField
+                value={formValue.consumerId}
+                onChange={(e) => {
+                setFormValue({ ...formValue, consumerId: e.target.value });
+                }}
+            />
+            </FieldContainer>
+        </FieldsContainer>
+      )}
+      {type ===
+        PanelTypes.FIRST_NAME_LAST_NAME_DATE_OF_BIRTH_AND_CONTRACT_ID && (
+        <FieldsContainer>
+          <FieldContainer>
+            <Label>First Name:</Label>
+            <TextField
+              value={formValue.firstName}
+              onChange={(e) => {
+                setFormValue({ ...formValue, firstName: e.target.value });
+              }}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Last Name:</Label>
+            <TextField
+              value={formValue.lastName}
+              onChange={(e) => {
+                setFormValue({ ...formValue, lastName: e.target.value });
+              }}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Date of Birth:</Label>
+            <TextField
+              value={formValue.dob}
+              onChange={(e) => {
+                setFormValue({ ...formValue, dob: e.target.value });
+              }}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Contract ID:</Label>
+            <TextField
+              value={formValue.contractId}
+              onChange={(e) => {
+                setFormValue({ ...formValue, contractId: e.target.value });
+              }}
+            />
+          </FieldContainer>
+        </FieldsContainer>
+      )}
+      {type === PanelTypes.CONTRACT_ID_AND_PERSON_NUMBER && (
+        <FieldsContainer>
+          <FieldContainer>
+            <Label>Contract ID:</Label>
+            <TextField
+              value={formValue.contractId}
+              onChange={(e) => {
+                setFormValue({ ...formValue, contractId: e.target.value });
+              }}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Person Number:</Label>
+            <TextField
+              value={formValue.personNumber}
+              onChange={(e) => {
+                setFormValue({ ...formValue, personNumber: e.target.value });
+              }}
+            />
+          </FieldContainer>
+        </FieldsContainer>
+      )}
+      {type ===
+        PanelTypes.FIRST_NAME_LAST_NAME_GROUP_NUMBER_AND_DATE_OF_BIRTH && (
+        <FieldsContainer>
+          <FieldContainer>
+            <Label>First Name:</Label>
+            <TextField
+              value={formValue.firstName}
+              onChange={(e) => {
+                setFormValue({ ...formValue, firstName: e.target.value });
+              }}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Last Name:</Label>
+            <TextField
+              value={formValue.lastName}
+              onChange={(e) => {
+                setFormValue({ ...formValue, lastName: e.target.value });
+              }}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Date of Birth:</Label>
+            <TextField
+              value={formValue.dob}
+              onChange={(e) => {
+                setFormValue({ ...formValue, dob: e.target.value });
+              }}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Group Number:</Label>
+            <TextField
+              value={formValue.groupNumber}
+              onChange={(e) => {
+                setFormValue({ ...formValue, groupNumber: e.target.value });
+              }}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Work Email:</Label>
+            <TextField
+              value={formValue.workEmail}
+              onChange={(e) => {
+                setFormValue({ ...formValue, workEmail: e.target.value });
+              }}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Work Phone:</Label>
+            <TextField
+              value={formValue.workPhone}
+              onChange={(e) => {
+                setFormValue({ ...formValue, workPhone: e.target.value });
+              }}
+            />
+          </FieldContainer>
+        </FieldsContainer>
+      )}
+    </div>
+  );
+}
